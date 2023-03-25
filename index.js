74e260d080140de6b62a34d0b9efcb97fea0f505
Jonas sousa <jonassousagtr@gmail.com>
2023-03-25 17:34:08 -0300

    Criando uma função que pega os dados da pasta arquivos e exibe no console, e tambem fiz uma função que trata o erro caso ocorra.

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+import fs from 'fs';
 import chalk from 'chalk';
 
-console.log(chalk.blue('olá mundo'));
-console.log('São geralmente recuperados')
+function trataErro(erro) {
+    throw new Error(chalk.red(erro.code, 'Não há aruivo no diretório'));
+}
 
+function pegaArquivo(caminhoDoArquivo) {
+    const encoding = 'utf-8';
+    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+            if (erro) {
+                trataErro(erro);
+            }
+        console.log(chalk.green(texto))
+    })
+}
+
+pegaArquivo('./arquivos/texto.md');
