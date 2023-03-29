da474251647fa715a055342c2a9d0f6cae57145d
Jonas sousa <jonassousagtr@gmail.com>
2023-03-29 20:25:10 -0300

    Criando um tratamento de erro de forma personalizada.

--- src/cli.js
+++ src/cli.js
@@ -4,13 +4,25 @@ import pegaArquivo from "./index.js";
 
 const caminho = process.argv;
 
-function imprimeLista(resultado) {
-    console.log(chalk.yellow('Lista de links'), resultado);
+function imprimeLista(resultado, identificador = '') {
+    console.log(
+        chalk.yellow('Lista de links'), 
+        chalk.black.bgGreen(identificador),
+        resultado);
 }
-
 
 async function processaTexto(arg) {
     const caminho = arg[2]
+ 
+    try {
+        fs.lstatSync(caminho)
+    } catch (erro) {
+        if(erro.code === 'ENOENT'){
+            console.log('Não existe o arquivo');
+            return
+        }
+    }
+
 
     if(fs.lstatSync(caminho).isFile()){
         const resultado = await pegaArquivo(arg[2])
@@ -19,7 +31,7 @@ async function processaTexto(arg) {
         const arquivos = await fs.promises.readdir(caminho)
         arquivos.forEach( async (nomeDeArquivo) => {
             const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`)
-            imprimeLista(lista)
+            imprimeLista(lista, nomeDeArquivo)
         })
     }
 }
