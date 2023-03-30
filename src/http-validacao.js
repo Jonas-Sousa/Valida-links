1b81f58380ed0077b0290abc0b6ece2183f105d8
Jonas sousa <jonassousagtr@gmail.com>
2023-03-29 21:29:44 -0300

    Agora a função faz as requisições e retorna os status code das mesmas.

--- src/http-validacao.js
+++ src/http-validacao.js
@@ -2,10 +2,22 @@ function extraiLinks (arrLinks) {
    return  arrLinks.map((objLink) => Object.values(objLink).join())
 }
 
+async function checaStatus (listURLs) {
+    const arrStatus = await Promise
+    .all(
+        listURLs.map(async (url) => {
+            const response = await fetch(url)
+            return response.status;
+        })
+    )
+    return arrStatus
+}
 
 
-function listaValidada (listaDeLinks) {
-    return extraiLinks(listaDeLinks)
+async function listaValidada (listaDeLinks) {
+   const links = extraiLinks(listaDeLinks)
+   const status = await checaStatus(links)
+   return status
 }
 
 export default listaValidada
